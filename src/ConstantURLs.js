36a87980f6e7491ee7fd390a80e19a228251bd1b
osha7 <ogroetz@gmail.com>
2021-03-02 12:16:41 -0700

    new backend url

--- src/ConstantURLs.js
+++ src/ConstantURLs.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:3000"
-export const API_URL = "https://og-shoppies-challenge-backend.herokuapp.com/"
+export const API_URL = "https://og-shoppies-challenge-backend1.herokuapp.com/"
